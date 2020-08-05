5857205c1a554834b77e0eb77e2ecc393936403a
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2020-08-05 17:35:13 -0600

    allow for different event types, begin room UI
    also tabs -> spaces

--- index.js
+++ index.js
@@ -2,65 +2,74 @@ const wsUri = "wss://pumtua6e6k.execute-api.us-east-1.amazonaws.com/beta"
 let ws = undefined
 
 const connect = () => {
-	const username = document.getElementById("username").value || "username"
-	ws = new WebSocket(wsUri + "?username=" + username)
+  const username = document.getElementById("username").value || "username"
+  ws = new WebSocket(wsUri + "?username=" + username)
 
-	ws.onopen = () => {
-		write("Connected as " + username)
-	}
-	ws.onclose = () => {
-		write("Disconnected")
-	}
-	ws.onmessage = (msg) => {
-		console.log(msg)
-		const message = JSON.parse(msg.data)
-		write(message)
-	}
-	ws.onerror = (err) => {
-		write("ERROR: " + err.data)
-	}
+  ws.onopen = () => {
+    write("Connected as " + username)
+  }
+  ws.onclose = () => {
+    write("Disconnected")
+  }
+  ws.onmessage = (msg) => {
+    const message = JSON.parse(msg.data)
+    switch (message.event) {
+      case "message":
+        console.log(message)
+        write(message.body)
+        break
+      case "rooms":
+        updateRooms(message.body)
+      default:
+        console.error("Unrecognized WebSocket event type:", message.event)  
+    }
+    
+  }
+  ws.onerror = (err) => {
+    write("ERROR: " + err.data)
+  }
 
-	document.getElementById("chatDiv").classList.remove("hidden")
-	document.getElementById("loginDiv").classList.add("hidden")
+  document.getElementById("chatDiv").classList.remove("hidden")
+  document.getElementById("loginDiv").classList.add("hidden")
 }
 
 const send = () => {
-	message = document.getElementById("message").value
-	if (message === "") return
+  message = document.getElementById("message").value
+  if (message === "") return
 
-	data = {
-		"action": "send",
-		"body": message
-	}
-	ws.send(JSON.stringify(data))
-	document.getElementById("message").value = ""
+  data = {
+    "action": "send",
+    "body": message
+  }
+  ws.send(JSON.stringify(data))
+  document.getElementById("message").value = ""
 }
 
 const write = (msg) => {
-	p = document.createElement("p")
-	text = document.createTextNode(msg)
-	p.appendChild(text)
+  p = document.createElement("p")
+  text = document.createTextNode(msg)
+  p.appendChild(text)
 
-	ouput = document.getElementById("output")
-	output.appendChild(p)
-	output.scrollTop = output.scrollHeight
+  ouput = document.getElementById("output")
+  output.appendChild(p)
+  output.scrollTop = output.scrollHeight
 }
 
 window.onload = function() {
-	document.getElementById("message").addEventListener("keyup",
-		function(event) {	
-			event.preventDefault()
-			if (event.key === "Enter") {
-				document.getElementById("sendButton").click()
+  document.getElementById("message").addEventListener("keyup",
+    function(event) { 
+      event.preventDefault()
+      if (event.key === "Enter") {
+        document.getElementById("sendButton").click()
 
-			}
-	});
-	document.getElementById("username").addEventListener("keyup",
-		function(event) {	
-			event.preventDefault()
-			if (event.key === "Enter") {
-				document.getElementById("loginButton").click()
+      }
+  });
+  document.getElementById("username").addEventListener("keyup",
+    function(event) { 
+      event.preventDefault()
+      if (event.key === "Enter") {
+        document.getElementById("loginButton").click()
 
-			}
-	});
+      }
+  });
 }
